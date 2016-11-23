8c03f50df51b0b00af29f6407cb549430f17c712
mryellow99 <ye01ll10ow@gmail.com>
2016-11-23 22:08:39 +0900

    自作ウィジェット追加

--- functions.php
+++ functions.php
@@ -13,7 +13,67 @@ function arphabet_widgets_init() {
 }
 add_action( 'widgets_init', 'arphabet_widgets_init' );
 
-/* eye-cathing image */
+/* 自作ウィジェット */
+class My_Widget extends WP_Widget {
+    /**
+     * Widgetを登録する
+     */
+    function __construct() {
+        $widget_ops = array('description' => 'プロフィールを設定できます');
+        parent::__construct(
+            "profile", //base ID
+            "プロフィール",
+            $widget_ops
+        );
+    }
+    /**
+     * 表側の Widget を出力する
+     *
+     * @param array $args  
+     * @param array $instance
+     */
+    function widget($args, $instance) {
+        // ウィジェットの表示処理を記述
+        $my_name = $instance['my_name'];
+        $my_text = $instance['my_text'];
+        echo $args['before_widget'];
+        echo $args['before_title'];
+        echo "プロフィール";
+        echo $args['after_title'];
+        echo "<p>name: ${my_name}</p>";
+        echo "<p>text: ${my_text}</p>";
+
+        echo $args['after_widget'];
+    }
+    function update($new_instance, $old_instance) {
+        // ウィジェットの設定の更新処理を記述。主にセキュリティを考えたサニタイズ用
+        return $new_instance;
+    }
+    function form($instance) {
+        // ウィジェットの設定用フォーム
+        $myname_value = $instance['my_name'];
+        $myname_name = $this->get_field_name('my_name');
+        $myname_id = $this->get_field_id('my_name');
+
+        $text_value = esc_attr($instance['my_text']);
+        $text_name = $this->get_field_name('my_text');
+        $text_id = $this->get_field_id('my_text');
+        
+        echo "<p>";
+        echo "<label for=\"{$myname_id}\">名前:</label>";
+        echo "<input class=\"widefat\" id=\"{$myname_id}\" name=\"{$myname_name}\" type=\"text\" value=\"{$myname_value}\">";
+        echo "</p>";
+
+        echo "<p>";
+        echo "<label for=\"{$text_id}\">紹介文:</label>";
+        echo "<textarea rows=\"3\" wrap=\"hard\" class=\"widefat\" id=\"{$text_id}\" name=\"{$text_name}\">{$text_value}</textarea>";
+        echo "</p>";
+    }
+}
+add_action('widgets_init',create_function('', 'return register_widget("My_Widget");'));
+
+
+/* アイキャッチ画像 */
 add_theme_support( 'post-thumbnails' );
 
 
