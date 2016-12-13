4355718ed81ece5ebd261e575f0c927bc33e788c
mryellow99 <ye01ll10ow@gmail.com>
2016-12-13 15:15:54 +0900

    記事ページ アイキャッチ画像 表示しないように コメントアウト

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
 		echo "<article>";
-        if ( has_post_thumbnail() ) {
+        /*if ( has_post_thumbnail() ) {
 		the_post_thumbnail();
-		}
+	}*/
         $title = get_the_title();
         echo "<h3>".$title."</h3>";
 		echo the_content();
