52f700b42efc7c3373e65149e345cc624d020b20
mryellow99 <ye01ll10ow@gmail.com>
2016-12-22 18:33:01 +0900

    ページ送りに関する記述削除

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ class My_Widget extends WP_Widget {
     /**
      * 表側の Widget を出力する
      *
-     * @param array $args  
+     * @param array $args
      * @param array $instance
      */
     function widget($args, $instance) {
@@ -58,7 +58,7 @@ class My_Widget extends WP_Widget {
         $text_value = esc_attr($instance['my_text']);
         $text_name = $this->get_field_name('my_text');
         $text_id = $this->get_field_id('my_text');
-        
+
         echo "<p>";
         echo "<label for=\"{$myname_id}\">名前:</label>";
         echo "<input class=\"widefat\" id=\"{$myname_id}\" name=\"{$myname_name}\" type=\"text\" value=\"{$myname_value}\">";
@@ -86,44 +86,5 @@ $custom_header_defaults = array(
     'header-text'            => true,//ヘッダー画像上にテキストをかぶせる
 );
 add_theme_support( 'custom-header', $custom_header_defaults );
-
-
-/* ページ送り */
-function pagination($pages = '', $range = 1)
-{
-     $showitems = ($range * 2)+1;  
- 
-     global $paged;
-     if(empty($paged)) $paged = 1;
- 
-     if($pages == '')
-     {
-         global $wp_query;
-         $pages = $wp_query->max_num_pages;
-         if(!$pages)
-         {
-             $pages = 1;
-         }
-     }   
- 
-     if(1 != $pages)
-     {
-         echo "<div class=\"pagination\"><div class=\"pagination-box\"><span class=\"page-of\">Page ".$paged." of ".$pages."</span>";
-         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo;</a>";
-         if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a>";
- 
-         for ($i=1; $i <= $pages; $i++)
-         {
-             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
-             {
-                 echo ($paged == $i)? "<span class=\"current\">".$i."</span>":"<a href='".get_pagenum_link($i)."' class=\"inactive\">".$i."</a>";
-             }
-         }
- 
-         if ($paged < $pages && $showitems < $pages) echo "<a href=\"".get_pagenum_link($paged + 1)."\">&rsaquo;</a>";
-         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>&raquo;</a>";
-         echo "</div></div>\n";
-     }
-}
 
 ?>
