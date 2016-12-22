34500a46392c561fc6dc8713a5a05ae6bba19c91
mryellow99 <ye01ll10ow@gmail.com>
2016-12-22 18:33:15 +0900

    ページ送り 書き直し

--- home.php
+++ home.php
@@ -24,9 +24,28 @@ if ( have_posts() ) {
 	echo wpautop( '投稿が見つかりませんでした。' );
 }
 
-if (function_exists("pagination")) {
-    pagination($additional_loop->max_num_pages);
+echo "<div class=\"pagination\">";
+global $wp_rewrite;
+$paginate_base = get_pagenum_link(1);
+if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
+    $paginate_format = '';
+    $paginate_base = add_query_arg('paged','%#%');
 }
+else{
+    $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+    user_trailingslashit('page/%#%/','paged');;
+    $paginate_base .= '%_%';
+}
+echo paginate_links(array(
+    'base' => $paginate_base,
+    'format' => $paginate_format,
+    'total' => $wp_query->max_num_pages,
+    'mid_size' => 4,
+    'current' => ($paged ? $paged : 1),
+    'prev_text' => '« 前へ',
+    'next_text' => '次へ »',
+));
+echo "</div>";
 
 echo "</article>";
 get_sidebar();
